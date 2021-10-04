32ca45822eda442a208820b9a7a8bb973ef8ec3f
hema <ibabedal@microsoft.com>
2021-10-04 16:21:23 +0300

    rebuilding the sssd database after joining

--- sles-domain-join.sh
+++ sles-domain-join.sh
@@ -12,65 +12,65 @@ domain_admin_username=$2
 domain_admin_password=$3
 no_caps_domain_name=${domain_name,,}
 
-function sles15_join_domain()
-{
-    hostname=`hostname`
-    #Take a backup of hosts file
-    cp /etc/hosts /etc/hosts-`date +"%d-%m-%y"`
-    echo "127.0.0.1    $hostname    $hostname.lab.local" >> /etc/hosts
-    echo "Updating the DNS server search" >> $logfile
-    cp /etc/sysconfig/network/config /etc/sysconfig/network/config-`date +"%d-%m-%y"`
-    sed -i 's/NETCONFIG_DNS_STATIC_SEARCHLIST=""/NETCONFIG_DNS_STATIC_SEARCHLIST="lab.local"/g' /etc/sysconfig/network/config
-    systemctl restart wicked.service
-    echo "Installing the required packages" >> $logfile
-    zypper --non-interactive install realmd sssd sssd-tools adcli krb5-client samba-client openldap2-client sssd-ad  >> $logfile
-    realm discover $domain_name >> $logfile
-    verify=`realm discover $domain_name | grep 'realm-name:' | cut -d : -f2 |  tr -d [:space:]`
-    nocaps_domain_name=`realm discover $domain_name | grep 'domain-name:' | cut -d : -f2 | tr -d [:space:]`
-    if [ $verify == $domain_name ] && [ $? == 0 ]
-    then
-        echo "Domain $verify was discovered" >> $logfile
-        echo "Updating the krb5.conf to fix the issue in : https://www.suse.com/support/kb/doc/?id=000018928" >> $logfile
-        echo "[libdefaults]" >> /etc/krb5.conf.d/domain_join.conf
-        echo "default_ccache_name = FILE:/tmp/krb5cc_%{uid}" >> /etc/krb5.conf.d/domain_join.conf
-        echo "Trying to check for the certificate for the admin user using kinit" >> $logfile
-        echo "$domain_admin_password" | kinit $domain_admin_username@$domain_name >> $logfile
-        if [ $? == 0 ]
-        then
-            echo "The credentials cache has been updated successfully" >> $logfile
-            echo "Waiting for 1 min before trying to join the machine to the domain" >> $logfile
-            sleep 60
-            echo "Trying to join domain now .." >> $logfile
-            echo "$domain_admin_password" | realm join --verbose $domain_name -U "$domain_admin_username@$domain_name" >> $logfile
-            if [ $? == 0 ]
-            then 
-                echo "Updating the sudo configuration to add the user to the sudo users" >> $logfile
-                echo "$domain_admin_username@$nocaps_domain_name   ALL=(ALL)    NOPASSWD:ALL" >> /etc/sudoers.d/domain-join
-                echo "Updating the config file for not to use the FQDN in user login" >> $logfile
-                cp /etc/sssd/sssd.conf /etc/sssd/sssd.conf-`date +"%d-%m-%y"`
-                sed -i '/^use_fully_qualified_names/ s/True/False/g ' sssd.conf
-                systemctl restart sssd
-                systemctl enable sssd
-                echo "Copying the krb5.conf and sssd.conf to the /root directory as a reference" >> $logfile
-                cp /etc/krb5.conf /root/
-                cp /etc/sssd/sssd.conf /root/
-                echo "We are successfully joined to the domain ^_^" >> $logfile
-                echo "Enjoy your day :) " >> $logfile
-                #echoing the messages for the stdout of custom script
-                echo "We are successfully joined to the domain ^_^"
-            else
-                echo "Failed to join the domain , please check the logs"
-                exit 3
-            fi
-        else
-        echo "Failed to refresh the credentials cache, please check the logs"
-        exit 2
-        fi
-    else
-    echo "Failed to discover the domain, check the network setup and try to run the script again"
-    exit 1
-    fi
-}
+#function sles15_join_domain()
+#{
+#    hostname=`hostname`
+#    #Take a backup of hosts file
+#    cp /etc/hosts /etc/hosts-`date +"%d-%m-%y"`
+#    echo "127.0.0.1    $hostname    $hostname.lab.local" >> /etc/hosts
+#    echo "Updating the DNS server search" >> $logfile
+#    cp /etc/sysconfig/network/config /etc/sysconfig/network/config-`date +"%d-%m-%y"`
+#    sed -i 's/NETCONFIG_DNS_STATIC_SEARCHLIST=""/NETCONFIG_DNS_STATIC_SEARCHLIST="lab.local"/g' /etc/sysconfig/network/config
+#    systemctl restart wicked.service
+#    echo "Installing the required packages" >> $logfile
+#    zypper --non-interactive install realmd sssd sssd-tools adcli krb5-client samba-client openldap2-client sssd-ad  >> $logfile
+#    realm discover $domain_name >> $logfile
+#    verify=`realm discover $domain_name | grep 'realm-name:' | cut -d : -f2 |  tr -d [:space:]`
+#    nocaps_domain_name=`realm discover $domain_name | grep 'domain-name:' | cut -d : -f2 | tr -d [:space:]`
+#    if [ $verify == $domain_name ] && [ $? == 0 ]
+#    then
+#        echo "Domain $verify was discovered" >> $logfile
+#        echo "Updating the krb5.conf to fix the issue in : https://www.suse.com/support/kb/doc/?id=000018928" >> $logfile
+#        echo "[libdefaults]" >> /etc/krb5.conf.d/domain_join.conf
+#        echo "default_ccache_name = FILE:/tmp/krb5cc_%{uid}" >> /etc/krb5.conf.d/domain_join.conf
+#        echo "Trying to check for the certificate for the admin user using kinit" >> $logfile
+#        echo "$domain_admin_password" | kinit $domain_admin_username@$domain_name >> $logfile
+#        if [ $? == 0 ]
+#        then
+#            echo "The credentials cache has been updated successfully" >> $logfile
+#            echo "Waiting for 1 min before trying to join the machine to the domain" >> $logfile
+#            sleep 60
+#            echo "Trying to join domain now .." >> $logfile
+#            echo "$domain_admin_password" | realm join --verbose $domain_name -U "$domain_admin_username@$domain_name" >> $logfile
+#            if [ $? == 0 ]
+#            then 
+#                echo "Updating the sudo configuration to add the user to the sudo users" >> $logfile
+#                echo "$domain_admin_username@$nocaps_domain_name   ALL=(ALL)    NOPASSWD:ALL" >> /etc/sudoers.d/domain-join
+#                echo "Updating the config file for not to use the FQDN in user login" >> $logfile
+#                cp /etc/sssd/sssd.conf /etc/sssd/sssd.conf-`date +"%d-%m-%y"`
+#                sed -i '/^use_fully_qualified_names/ s/True/False/g ' sssd.conf
+#                systemctl restart sssd
+#                systemctl enable sssd
+#                echo "Copying the krb5.conf and sssd.conf to the /root directory as a reference" >> $logfile
+#                cp /etc/krb5.conf /root/
+#                cp /etc/sssd/sssd.conf /root/
+#                echo "We are successfully joined to the domain ^_^" >> $logfile
+#                echo "Enjoy your day :) " >> $logfile
+#                #echoing the messages for the stdout of custom script
+#                echo "We are successfully joined to the domain ^_^"
+#            else
+#                echo "Failed to join the domain , please check the logs"
+#                exit 3
+#            fi
+#        else
+#        echo "Failed to refresh the credentials cache, please check the logs"
+#        exit 2
+#        fi
+#    else
+#    echo "Failed to discover the domain, check the network setup and try to run the script again"
+#    exit 1
+#    fi
+#}
 
 function sles12_join_domain()
 {
@@ -172,7 +172,10 @@ EOF
         if [ $? == 0 ]
         then
             echo "Updating the sudo configuration to add the user to the sudo users" >> $logfile
-            echo "$domain_admin_username@$nocaps_domain_name   ALL=(ALL)    NOPASSWD:ALL" >> /etc/sudoers.d/domain-join
+            echo "$domain_admin_username@$no_caps_domain_name   ALL=(ALL)    NOPASSWD:ALL" >> /etc/sudoers.d/domain-join
+            echo "Stopping and disabling nscd service" >> $logfile
+            systemctl stop nscd 
+            systemctl disable nscd 
             sssd config-check >> $logfile
             echo "Update the PAM configuration " >> $logfile
             pam-config -a --sss
@@ -183,6 +186,11 @@ EOF
             echo "Copying the krb5.conf and sssd.conf to the /root directory as a reference" >> $logfile
             cp /etc/krb5.conf /root/
             cp /etc/sssd/sssd.conf /root/
+            echo "Waiting for 1 min before rebuilding the sssd database " >> $logfile
+            sleep 60
+            systemctl stop sssd 
+            rm /var/lib/sss/db/*
+            systemctl start sssd
             echo "We are successfully joined to the domain ^_^" >> $logfile
             echo "Enjoy your day :) " >> $logfile
             #echoing the messages for the stdout of custom script
