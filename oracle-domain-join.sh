0a1ececd9052ac083c286933e76b906343eeeb6f
hema <ibabedal@microsoft.com>
2021-10-05 18:34:45 +0300

    adding the support for not to use FQDN for login

--- oracle-domain-join.sh
+++ oracle-domain-join.sh
@@ -40,7 +40,7 @@ function oracle7_join_domain()
                 echo "$domain_admin_username@$nocaps_domain_name   ALL=(ALL)    NOPASSWD:ALL" >> /etc/sudoers.d/domain-join
                 echo "Updating the config file for not to use the FQDN in user login" >> $logfile
                 cp /etc/sssd/sssd.conf /etc/sssd/sssd.conf-`date +"%d-%m-%y"`
-                sed -i '/^use_fully_qualified_names/ s/True/False/g ' sssd.conf
+                sed -i '/^use_fully_qualified_names/ s/True/False/g ' /etc/sssd/sssd.conf
                 systemctl restart sssd
                 systemctl enable sssd
                 echo "Copying the krb5.conf and sssd.conf to the /root directory as a reference" >> $logfile
